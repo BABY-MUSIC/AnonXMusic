9e7c8cf9b20dd145aa19b690165f01d2787f2c46
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2026-03-18 21:48:21 +0530

    Update buttons.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- anony/helpers/buttons.py
+++ anony/helpers/buttons.py
@@ -1,2 +1,42 @@
-def start_buttons():
-    return [[{"text":"Support","url":"https://t.me"}]]
+from anony import config
+
+
+# =====================
+
+def start_key(lang, private: bool):
+
+    if private:
+
+        keyboard = [
+            [
+                {
+                    "text": lang["add_me"],
+                    "url": f"https://t.me/{config.BOT_NAME}?startgroup=true",
+                }
+            ],
+            [
+                {
+                    "text": lang["support"],
+                    "url": config.SUPPORT_CHAT,
+                },
+                {
+                    "text": lang["help"],
+                    "callback_data": "help",
+                },
+            ],
+        ]
+
+    else:
+
+        keyboard = [
+            [
+                {
+                    "text": lang["support"],
+                    "url": config.SUPPORT_CHAT,
+                }
+            ]
+        ]
+
+    return {
+        "inline_keyboard": keyboard
+    }
