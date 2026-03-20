e93e9cc13ee3bcdbec90053b093a4d90165b72cd
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2026-03-20 10:49:31 +0530

    Update __init__.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- anony/database/__init__.py
+++ anony/database/__init__.py
@@ -1,1 +1,25 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from anony.config import MONGO_URL
+
+from .stream import init_stream
+from .player import init_player
+from .calls import init_calls
+from .queue import init_queue
+
+
 db = None
+
+
+async def init_db():
+
+    global db
+
+    client = AsyncIOMotorClient(MONGO_URL)
+
+    db = client.anony
+
+    init_stream(db)
+    init_player(db)
+    init_calls(db)
+    init_queue(db)
