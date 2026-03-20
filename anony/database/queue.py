902874c867860653f898cedd5ccfa07fc8617690
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2026-03-20 11:23:21 +0530

    Update queue.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- anony/database/queue.py
+++ anony/database/queue.py
@@ -7,6 +7,12 @@ def init_queue(database):
     global db
     global queue
 
+    print("INIT_QUEUE CALLED")
+
     db = database
 
+    print("DB =", db)
+
     queue = db.queue
+
+    print("QUEUE =", queue)
